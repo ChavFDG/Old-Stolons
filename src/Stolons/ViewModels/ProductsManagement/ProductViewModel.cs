88368ad28f7cb86d2ecd22bd1805cf84abd8c1b3
Dyslesiq <nicolas.michon@zoho.com>
2016-07-22 23:02:37 +0200

    Ajout du ProductViewModel

---
+++ src/Stolons/ViewModels/ProductsManagement/ProductViewModel.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNet.Http;
+using Microsoft.Data.Entity;
+using Stolons.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stolons.ViewModels.ProductsManagement
+{
+    public class ProductViewModel
+    {
+
+	public Product Product { get; set; }
+
+	public string OrderedQuantityString;
+
+	public ProductViewModel()
+        {
+        }
+
+        public ProductViewModel(Product product, int orderedQty)
+        {
+	    Product = product;
+	    OrderedQuantityString = product.GetQuantityString(orderedQty);
+	}
+    }
+}
